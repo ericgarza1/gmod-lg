c54caddf68cdc119af572befd9c2e6033f153f3f
Eric Garza <116322897+ericgarza1@users.noreply.github.com>
2022-10-21 08:50:57 -0500

    Add files via upload

---
+++ lg_bs.lua
@@ -0,0 +1,9 @@
+--[[-------------------------------------------------------------------
+		Prints some feedback saying the Animation Extension successfully loaded.
+		
+	Contact: https://steamcommunity.com/groups/LazarusGamingGM
+		
+----------------------------------------]]--
+
+wOS.AnimExtension.Mounted[ "Blade Symphony" ] = true
+MsgC( Color( 255, 255, 255 ), "[LG] Successfully mounted animation extension: Blade Symphony\n" )
